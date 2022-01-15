df986f4ec25ff1b220d29e09dbecefad59d44a7f
YandroRB <santiago.rdbn@outlook.com>
2022-01-15 03:01:27 -0500

    Implementacion de barra de busquedad

--- app.js
+++ app.js
@@ -1,26 +1,80 @@
 //Variables
-const data={};
+const data=[];
 const weather={};
 const fecha=new Date();
 
 //Utilizacion del DOM Y BOM
 window.onload= async function(){
    await getLocation(data);
-   await getWeatherLocation(data,weather);
+   await getWeatherLocation(data.loc,weather);
    console.log(data)
    console.log(weather)
    const {current,location}=weather
     const view={
-       ubicacion:data.city,
+       ubicacion:`${location.region}/${location.name}`,
        status:current.condition.icon.replace('64x64','128x128'),
-       temperature:current.temp_c,
-       feel_like:current.feelslike_c,
-       tiempo:getTiempo(fecha.getHours()),
+       temperature:`${current.temp_c}°C`,
+       feel_like:`${current.feelslike_c}°C`,
+       wind:`${current.wind_dir} ${current.wind_kph}Km/h`,
+       humidicity:`${current.humidity}%`,
+       precipitacion:`${current.precip_mm}mm`,
        dia:getDayName(fecha.getDay()),
        fecha:fecha.toLocaleDateString('es-ES',{year:"numeric",month:"long",day:"numeric"})
    }
    loaderData(view)
    clearLoader(); 
+}
+const ciudad=document.getElementById('ciudad');
+const resultElement=document.getElementById('resultados');
+ciudad.addEventListener('keyup',async (e)=>{
+    e.preventDefault();
+    const city=ciudad.value;
+    let resultados={};
+    if(city.length>2){
+        await searchWeather(city,resultados);
+        showResult(resultados,resultElement);
+    }
+})
+
+
+function showResult(resultados,contenedor){
+
+    if(Object.keys(resultados).length == 0){
+        return;
+    }
+    contenedor.innerHTML='';
+
+    Object.entries(resultados).forEach(([key,value])=>{
+        const li=document.createElement('li');
+        const a=document.createElement('a');
+        a.textContent=`${value.name} / ${value.region} / ${value.country}`;
+        a.href='#'
+        li.id=value.url;
+        li.addEventListener('click',mostrar)
+        li.appendChild(a);
+        contenedor.appendChild(li);
+
+    })
+}
+
+async function mostrar(event){
+    const weatherData={}
+    const address=event.target.parentNode.id
+    await getWeatherLocation(address,weatherData)
+    const {current,location}=weatherData
+    const view={
+        ubicacion:`${location.region}/${location.name}`,
+        status:current.condition.icon.replace('64x64','128x128'),
+        temperature:`${current.temp_c}°C`,
+        feel_like:`${current.feelslike_c}°C`,
+        wind:`${current.wind_dir} ${current.wind_kph}Km/h`,
+        humidicity:`${current.humidity}%`,
+        precipitacion:`${current.precip_mm}mm`,
+        dia:getDayName(fecha.getDay()),
+        fecha:fecha.toLocaleDateString('es-ES',{year:"numeric",month:"long",day:"numeric"})
+    }
+    console.log(view)
+    loaderData(view)
 }
 
 function loaderData(data){
@@ -28,7 +82,7 @@ function loaderData(data){
         if(dat=='status'){
             document.getElementById(dat).src=data[dat]
         }
-        document.getElementById(dat).textContent=data[dat]
+        document.getElementById(dat).textContent+=data[dat]
     })
 
 }
@@ -40,6 +94,20 @@ function clearLoader(){
 
 
 //Funciones
+//Funcion para buscar el clima de una ciudad
+async function searchWeather(city,resultados){
+    const API_KEY='5cf355d1310f419a91c52809210712';
+    const url=`https://api.weatherapi.com/v1/search.json?key=${API_KEY}&q=${city}`;
+    await fetch(url)
+    .then(res=>res.json())
+    .then(data=>{
+        for(let key in data){
+            resultados[key]=data[key];
+        }
+    })
+    .catch(err=>console.log(err))  
+}
+
 //Obtiene la localizacion del usuario
 async function getLocation(data){
     await fetch("https://ipinfo.io/json?token=64a2ce40924112")
@@ -53,9 +121,8 @@ async function getLocation(data){
 }
 //Obtiene el clima de la zona
 async function getWeatherLocation(data,weather){
-    const latLon=data.loc;
     const API_KEY='5cf355d1310f419a91c52809210712';
-    await fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${latLon}&aqi=yes`)
+    await fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${data}&aqi=yes`)
     .then((response)=>response.json())
     .then((jsonResponse)=>{
         for(let key in jsonResponse){
@@ -63,10 +130,10 @@ async function getWeatherLocation(data,weather){
         }
     })
 }
-function getDayName(day){
+ function getDayName(day){
     const names=['Domingo','Lunes','Martes','Miercoles','Jueves','Viernes','Sabado'];
     return names[day]
-}
+}/*
 function getTiempo(hora){
     if(hora>6&&hora<12){
         return 'Buenos días';
@@ -80,4 +147,4 @@ function getTiempo(hora){
     else{
         return 'Buenas madrugadas';
     }
-}
+} */
